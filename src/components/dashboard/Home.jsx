385cc6ef7a70d21054a7ea9f05e893f1d5d2d126
Chance <1565222098@qq.com>
2018-04-16 00:15:25 +0800

    优化主页

--- src/components/dashboard/Home.jsx
+++ src/components/dashboard/Home.jsx
@@ -10,6 +10,7 @@ import b1 from '../../style/imgs/b1.jpg';
 
 import Content1 from './Content1';
 import Content2 from './Content2';
+import Content3 from './Content3';
 import '../../style/less/antMotion_style.less';
 
 const scrollScreen = ScrollAnim.scrollScreen;
@@ -72,6 +73,7 @@ export default class Home extends React.Component {
                                     <AutoPlay />
                                     <Content1 id="content_2_0" key="content_2_0" isMode={this.state.isMode}/>
                                     <Content2 id="content_3_0" key="content_3_0" isMode={this.state.isMode}/>
+                                    <Content3 id="content_4_0" key="content_4_0" isMode={this.state.isMode}/>
                                 </Card>
                             }
                         </div>
