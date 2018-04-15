cf9445d55175399797f9d28ccaa434e22b2416e3
Chance <1565222098@qq.com>
2018-04-15 23:18:44 +0800

    优化首页，使滑动效果慢加载

--- src/components/dashboard/Home.jsx
+++ src/components/dashboard/Home.jsx
@@ -3,7 +3,7 @@
  */
 import React from 'react';
 import { Row, Col, Card } from 'antd';
-import enquire from 'enquire';
+import enquire from 'enquire.js';
 import ScrollAnim from 'rc-scroll-anim';
 import AutoPlay from '../ui/banners/AutoPlay';
 import b1 from '../../style/imgs/b1.jpg';
@@ -14,7 +14,7 @@ import '../../style/less/antMotion_style.less';
 
 const scrollScreen = ScrollAnim.scrollScreen;
 
-let isMobile;
+// let isMobile;
 // enquireScreen((b) => {
 //   isMobile = b;
 // });
@@ -23,24 +23,24 @@ export default class Home extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-          isMobile: false,
+          isMode: false,
           show: false,
         };
     }
     componentDidMount() {
         // 适配手机屏幕;
-        enquire.register('screen and (max-width: 900px)',{
-            match(e){
-                isMobile = true;
-            },
-            unmatch(e){
-                isMobile = false;
-            }
-        });
-        this.setState({ isMobile: true });
-        // enquireScreen((b) => {
-        //   this.setState({ isMobile: !!b });
+        // enquire.register('screen and (max-width: 900px)',{
+        //     match(e){
+        //         isMobile = true;
+        //     },
+        //     unmatch(e){
+        //         isMobile = false;
+        //     }
         // });
+        // this.setState({ isMobile: isMobile });
+        this.enquireScreen((isMode) => {
+          this.setState({ isMode });
+        });
         // dva 2.0 样式在组件渲染之后动态加载，导致滚动组件不生效；线上不影响；
         // if (location.port) {
           // 样式 build 时间在 200-300ms 之间;
@@ -51,6 +51,16 @@ export default class Home extends React.Component {
           }, 500);
         // }
     }
+    enquireScreen = (cb) => {
+        enquire.register('screen and (max-width: 767px)',{
+            match: () => {
+                cb && cb(true);
+            },
+            unmatch: () => {
+                cb && cb();
+            }
+        });
+    }
     render() {
         return (
             <div className="gutter-example button-demo">
@@ -60,8 +70,8 @@ export default class Home extends React.Component {
                             { this.state.show &&
                                 <Card bordered={false}>
                                     <AutoPlay />
-                                    <Content1 id="content_2_0" key="content_2_0" isMode={this.state.isMobile}/>
-                                    <Content2 id="content_3_0" key="content_3_0" isMode={this.state.isMobile}/>
+                                    <Content1 id="content_2_0" key="content_2_0" isMode={this.state.isMode}/>
+                                    <Content2 id="content_3_0" key="content_3_0" isMode={this.state.isMode}/>
                                 </Card>
                             }
                         </div>
